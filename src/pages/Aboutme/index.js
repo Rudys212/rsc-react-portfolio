7f4d0a840b89503e5064ce9d12f096eee7ebcdb7
Rudy Sanchez <rudys212@gmail.com>
2021-09-01 02:00:35 -0700

    added semantic ui

---
+++ src/pages/Aboutme/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+import ProfilePic from "../../assets/images/Profile pic.jpg";
+
+export default function Aboutme() {
+  return (
+    <div>
+      <Container text fluid className="aboutmesection">
+        <h1 id="aboutmetitle">Hi there, my name is Rudy Sanchez Cisneros!</h1>
+        <img
+          class="headshot"
+          src={ProfilePic}
+          alt="my very professional headshot"
+        />
+        <h2 id="aboutMeHeader"> About Me:</h2>
+        <div id="aboutme">
+          <p id="paraTxtOne">
+            I am a Front-End web developer with a background in Food and
+            Nutrition science. Currently, completing a full stack web
+            development program at the University of Washington. Experience
+            working in teams or individually to meet goals. Have worked with
+            HTML, CSS and JavaScript to build full-stack applications that are
+            efficient and interactive.
+          </p>
+          <br />
+          <p id="paraTxtTwo">
+            I create and develop web applications with the user in mind. I like
+            my designs to be easy to use and navigate as well as having a clean
+            UI. I believe in creating projects that will meet both, my clients'
+            needs and those of their users.
+          </p>
+        </div>
+        <br />
+      </Container>
+    </div>
+  );
+}
--- src/pages/Mywork/index.js
+++ src/pages/Mywork/index.js
@@ -1,1 +1,6 @@
-import Projects from "./components/Projects";
+import Projects from "../../components/Projects/index";
+
+export default function Mywork() {
+  <Projects />;
+  return <h1>My Work</h1>;
+}
